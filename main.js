10e28efc2b640d8a11718458a22d1136f6e46b74
Sujan99022 <sujanrumakantha@gmail.com>
2022-09-13 06:17:44 -0800

    new 1.1

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ if (visitCount) {
   localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
+
+        const search=()=>{
+            const searchBox = document.getElementById("search-item").value.toUpperCase();
+            const product = document.querySelectorAll(".con");
+            const pname = document.getElementsByTagName("h5");
+            for(var i = 0; i < pname.length; i++){
+                let match = product[i].getElementsByTagName("h5")[0];
+
+                if(match){
+                    let textValue = match.textContent || match.innerHTML
+
+                    if(textValue.toUpperCase().indexOf(searchBox) > -1){
+                      product[i].style.display = "";
+                    }
+                    else{
+                      product[i].style.display = "none";
+                    }
+                }
+            }
+        }
